2723894000c100365a9a8aff1d2ff99208e174dc
pandasprog <52864262+pandasprog@users.noreply.github.com>
2020-06-06 13:05:33 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+var gameData = {
+    stars : 0,
+    possiblecollapsedstars : 0,
+    SNC : 0,
+}
+
+setInterval(OnTick, 1)
+
+function OnTick() {
+    document.getElementById("ActualVal").innerHTML = gameData.stars + " Stars"
+    gameData.possiblecollapsedstars = Math.floor(gameData.stars/5) * ((gameData.stars%5) + 1)
+    if (gameData.possiblecollapsedstars > 0) {
+        document.getElementById("csb").innerHTML = "Collapse For " + gameData.possiblecollapsedstars + " Super Nova Coins"
+    }
+    else {
+        document.getElementById("csb").innerHTML = "Get to 5 Stars to Collapse"
+    }
+    if (gameData.SNC > 0) {
+        document.getElementById("Tab2butt").style.display = "inline-block"
+    }
+}
+
+function AddStar() {
+    gameData.stars += 1;
+}
+
+function CollapseStars() {
+    if (gameData.stars > 4) {
+        gameData.stars = 0
+        gameData.SNC += gameData.possiblecollapsedstars
+    }
+}
+
+function Tab1() {
+    document.getElementById("Tab1").style.display = "block"
+    document.getElementById("Tab2").style.display = "none"
+    document.getElementById("Tab3").style.display = "none"
+}
+
+function Tab2() {
+    document.getElementById("Tab1").style.display = "none"
+    document.getElementById("Tab2").style.display = "block"
+    document.getElementById("Tab3").style.display = "none"
+}
+
+function Tab3() {
+    document.getElementById("Tab1").style.display = "none"
+    document.getElementById("Tab2").style.display = "none"
+    document.getElementById("Tab3").style.display = "block"
+}
