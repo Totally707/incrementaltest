f1e5a762558306e84837980557b279ad9cfb4df0
pandasprog <52864262+pandasprog@users.noreply.github.com>
2020-09-15 17:09:57 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -9,14 +9,18 @@ setInterval(OnTick, 1)
 function OnTick() {
     document.getElementById("ActualVal").innerHTML = gameData.stars + " Stars"
     gameData.possiblecollapsedstars = Math.floor(gameData.stars/5) * ((gameData.stars%5) + 1)
-    if (gameData.possiblecollapsedstars > 0) {
-        document.getElementById("csb").innerHTML = "Collapse For " + gameData.possiblecollapsedstars + " Super Nova Coins"
+    if (gameData.possiblecollapsedstars > 1) {
+        document.getElementById("csb").innerHTML = "Collapse For " + gameData.possiblecollapsedstars + " Coins"
+    }
+    else if (gameData.possiblecollapsedstars = 1) {
+        document.getElementById("csb").innerHTML = "Collapse For 1 Coin"
     }
     else {
         document.getElementById("csb").innerHTML = "Get to 5 Stars to Collapse"
     }
     if (gameData.SNC > 0) {
         document.getElementById("Tab2butt").style.display = "inline-block"
+        document.getElementById("Tab2butt").innerHTML = "PLACEHOLDER TEXT"
     }
 }
 
